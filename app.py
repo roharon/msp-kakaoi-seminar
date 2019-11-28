3074d79ae2e321ecd7afaad8fe8e9763ed18fca3
Aaron Roh <roharon98@gmail.com>
2019-11-28 23:51:00 +0900

    fix: SimpleText

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def test():
 def skill():
     print(request.json)
     data = {
-        {
         "version": "2.0",
         "template": {
         "outputs": [
@@ -30,10 +29,9 @@ def skill():
                     "text": "간단한 텍스트 요소입니다."
                     }
                 }
-                ]
+]
             }
         }
-    }
     
     return jsonify(data)
     
