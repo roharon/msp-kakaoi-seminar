abba2b409c1d1dfb6388487334ac7c589a3a1bd3
Aaron Roh <roharon98@gmail.com>
2019-11-29 14:59:20 +0900

    add: QuickReplies

--- app.py
+++ app.py
@@ -11,61 +11,26 @@ def skill():
         "template": {
             "outputs": [
                 {
-                    "carousel": {
-                        "type": "commerceCard",
-                        "items": [
-                            {
-                                "title": "서브웨이",
-                                "description": "서브웨이입니다",
-                                "price": 1000,
-                                "discount": 200,
-                                "currency": "won",
-                                "thumbnails": [
-                                    {
-                                        "imageUrl": "http://subway.co.th/en/images/menu/catering/catering-mealbox.jpg",
-                                        "link": {
-                                            "web": "http://subway-menu.com/subway-menu-germany/subway-to-go-box/"
-                                        }
-                                    }
-                                ],
-                                "buttons": [
-                                    {
-                                        "label": "010-0000-0000에 전화걸기",
-                                        "action": "phone",
-                                        "phoneNumber": "010-0000-0000"
-                                    },
-                                    {
-                                        "label": "공유하기",
-                                        "action": "share"
-                                    }
-
-                                ]
-                            },
-                            {
-                                "title": "던킨도넛",
-                                "description": "던킨도넛입니다",
-                                "price": 3000,
-                                "discount": 200,
-                                "currency": "won",
-                                "thumbnails": [
-                                    {
-                                        "imageUrl": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/munckin-1548961144.jpg?crop=1xw:1xh;center,top&resize=480:*",
-                                        "link": {
-                                            "web": "https://news.dunkindonuts.com"
-                                        }
-                                    }
-                                ],
-                                "buttons": [
-                                    {
-                                        "label": "공유하기",
-                                        "action": "share"
-                                    }
-
-                                ]
-                            }
-                        ],
-
+                    "simpleText": {
+                        "text": "아래 노란버튼이 퀵리플라이!"
                     }
+                }
+            ],
+            "quickReplies": [
+                {
+                    "label": "퀵리플라이1",
+                    "action": "message",
+                    "messageText": "퀵리플라이1 응답메세지"
+                },
+                {
+                    "label": "퀵리플라이2",
+                    "action": "message",
+                    "messageText": "퀵리플라이2 응답메세지"
+                },
+                {
+                    "label": "퀵리플라이3",
+                    "action": "message",
+                    "messageText": "퀵리플라이3 응답메세지"
                 }
             ]
         }
