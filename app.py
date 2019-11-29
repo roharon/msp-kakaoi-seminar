96c3fcc0cd365fdeed1a205c311ed3347747eae0
Aaron Roh <roharon98@gmail.com>
2019-11-29 13:48:52 +0900

    add: CommerceCard

--- app.py
+++ app.py
@@ -11,18 +11,19 @@ def skill():
         "template": {
             "outputs": [
                 {
-                    "basicCard":{
-                        "title": "카드의 제목",
-                        "description": "상세 설명",
-                        "thumbnail": {
-                            "imageUrl": "https://t1.daumcdn.net/friends/www/talk/kakaofriends_talk_2018.png",
-                            "link": {
-                                "mobile": "https://naver.com",
-                                "android": "https://google.com",
-                                "web": "https://daum.net",
-                                "pc": "https://github.com"
-                                }
-                            },
+                    "commerceCard": {
+                        "description": "서브웨이",
+                        "price": 1000,
+                        "discountRate": 10,
+                        "discountedPrice": 900
+                        "currency": "won",
+                        "thumbnails": [
+                            {
+                                "imageUrl": "http://subway.co.th/en/images/menu/catering/catering-mealbox.jpg",
+                                "link": {
+                                    "web": "http://subway-menu.com/subway-menu-germany/subway-to-go-box/"
+                            }
+                        ],
                         "buttons": [
                             {
                                 "label": "010-0000-0000에 전화걸기",
@@ -33,12 +34,12 @@ def skill():
                                 "label": "공유하기",
                                 "action": "share"
                             }
-                           
+
                         ]
                     }
                 }
             ]
-        }   
+        }
     }
 
     return jsonify(data)
