e89b7ba555c08bb953e1c6391ada7756411af9d8
Aaron Roh <roharon98@gmail.com>
2019-11-29 12:37:59 +0900

    add: basiccard

--- app.py
+++ app.py
@@ -11,9 +11,20 @@ def skill():
         "template": {
             "outputs": [
                 {
-                    "simpleImage": {
-                        "imageUrl": "http://builder.hufs.ac.kr/user/hufs/mycodyimages/new/goimg_navy.png",
-                        "altText": "HUFS 로고"
+                    "basicCard":{
+                        "title": "카드의 제목",
+                        "description": "상세 설명",
+                        "thumbnail": {
+                            "imageUrl": "https://t1.daumcdn.net/friends/www/talk/kakaofriends_talk_2018.png"
+                            },
+                        "buttons": [
+                            {
+                                "label": "첫번째 버튼",
+                                "action": "message",
+                                "messageText": "첫번째 버튼을 눌렀습니다!"
+                            }
+                           
+                        ]
                     }
                 }
             ]
