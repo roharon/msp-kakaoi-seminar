6415eee3ba40fa0b9ccea3006ff3f8ffa6346917
Aaron Roh <roharon98@gmail.com>
2019-11-29 13:06:50 +0900

    add: web

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def skill():
                             "imageUrl": "https://t1.daumcdn.net/friends/www/talk/kakaofriends_talk_2018.png",
                             "link": {
                                 "mobile": "https://naver.com",
+                                "web": "https://hufs.ac.kr"
                                 }
                             },
                         "buttons": [
